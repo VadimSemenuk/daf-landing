d65a3bc83fec0257edafe36581abe60350a306d5
Vadim Semenyuk <vadim54787@gmail.com>
2026-02-01 21:12:23 +0300

    dev

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,9 +2,15 @@
 import { defineConfig } from 'astro/config';
 import tailwindcss from "@tailwindcss/vite";
 
+import sitemap from '@astrojs/sitemap';
+
 // https://astro.build/config
 export default defineConfig({
-    vite: {
-        plugins: [tailwindcss()],
-    },
+  site: 'https://daf.pragmatsoft.com',
+
+  vite: {
+      plugins: [tailwindcss()],
+  },
+
+  integrations: [sitemap()],
 });
